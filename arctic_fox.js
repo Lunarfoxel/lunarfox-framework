dd9146b683c320ad89c4f5ab277e95edde7c8531
RiftyM <RiftyM@users.noreply.github.com>
2017-06-01 10:31:02 +0200

    Create arctic_fox.js

---
+++ arctic_fox.js
@@ -0,0 +1,45 @@
+
+//LunarFox Framework (C) Copyright 2017 Mike Rifty. All Rights Reserved.
+//
+//Alpha Stage Version, all modules to be consolidated into a single js file and minified
+//
+//Arctic Fox module [Version 0.1]
+//
+//
+//Future enhancements will include jQuery animations for smoother effects
+
+function makeFlurry(element, amount, timer) {
+  
+  //Set variables
+  //123...
+  
+  //Check if both arguments are valid
+  
+  if (!element || !amount) {
+    alert("Error: invalid makeFlurry() arguments"); //Testing to be improved in future
+  }
+  
+  setInterval(function() {
+        
+    //Code to implement flurries goes here...
+    
+  }, timer);
+    
+}; 
+
+function makeFrost(element, color) {
+  
+    if (!element || !color) {
+      alert("Error: invalid makeFrost() arguments"); //Testing to be improved in future
+    }
+  
+  
+  //Code goes here...
+  
+};
+
+function makeSnow() {
+  
+  //Code goes here...
+  
+};
