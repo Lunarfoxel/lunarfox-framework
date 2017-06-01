1dfde7a89f8fd01a2a6b35419a6f38644dbd86d3
RiftyM <RiftyM@users.noreply.github.com>
2017-06-01 10:32:59 +0200

    Create lava_dragon.js

---
+++ lava_dragon.js
@@ -0,0 +1,45 @@
+
+//LunarFox Framework (C) Copyright 2017 Mike Rifty. All Rights Reserved.
+//
+//Alpha Stage Version, all modules to be consolidated into a single js file and minified
+//
+//Lava Dragon module [Version 0.1]
+//
+//
+//Future enhancements will include jQuery animations for smoother effects
+
+function makeFire(element, amount, timer) {
+  
+  //Set variables
+  //123...
+  
+  //Check if both arguments are valid
+  
+  if (!element || !amount) {
+    alert("Error: invalid makeFire() arguments"); //Testing to be improved in future
+  }
+  
+  setInterval(function() {
+        
+    //Code to implement fire goes here...
+    
+  }, timer);
+    
+}; 
+
+function makeLava(element, color) {
+  
+    if (!element || !color) {
+      alert("Error: invalid makeLava() arguments"); //Testing to be improved in future
+    }
+  
+  
+  //Code goes here...
+  
+};
+
+function makeSmoke() {
+  
+  //Code goes here...
+  
+};
