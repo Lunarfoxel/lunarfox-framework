50a6d1751dd328d14a11a1d959a216f173e4c281
RiftyM <RiftyM@users.noreply.github.com>
2017-06-01 10:35:45 +0200

    Create rainforest_pegasus.js

---
+++ rainforest_pegasus.js
@@ -0,0 +1,49 @@
+
+//LunarFox Framework (C) Copyright 2017 Mike Rifty. All Rights Reserved.
+//
+//Alpha Stage Version, all modules to be consolidated into a single js file and minified
+//
+//Rainforest Pegasus module [Version 0.1]
+//
+//
+//Future enhancements will include jQuery animations for smoother effects
+
+function makeCloud(element, type, amount) {
+  
+  //Set variables
+  //123...
+  
+  //Check if arguments are valid
+  
+  if (!element || !amount || !amount) {
+    alert("Error: invalid makeCloud() arguments"); //Testing to be improved in future
+  }
+  
+  setInterval(function() {
+        
+    //Code to implement flurries goes here...
+    
+  }, timer);
+    
+}; 
+
+function makeRain(direction, amount) {
+  
+    if (!direction || !amount) {
+      alert("Error: invalid makeRain() arguments"); //Testing to be improved in future
+    }
+  
+  
+  //Code goes here...
+  
+};
+
+function makeWind(direction, amount) {
+  
+    if (!direction || !amount) {
+      alert("Error: invalid makeWind() arguments"); //Testing to be improved in future
+    }  
+
+  //Code goes here...
+  
+};
